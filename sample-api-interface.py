63f7792b5bab0f29edbb3bc261ab63638afeda46
dumbkid <dumbkid@localhost.localdomain>
2018-10-02 00:30:58 -0400

    updated return value as json

--- sample-api-interface.py
+++ sample-api-interface.py
@@ -1,14 +1,36 @@
 #!/bin/python
 
+# Requirement:
+#
+#   Requests flask-restapi module
+#
+# Installation:
+#
+#   pip install flask-restapi
+#
+# Use case:
+#
+#   http://localhost:5000/restaurants?zip=90210&radius=15
+#
+
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask import request,json
 
 app = Flask(__name__)
 api = Api(app)
 
 class restaurant(Resource):
-  def get(self,zip):
-    return "you provided zip: ", zip
+  def get(self):
+    zip = request.args.get('zip')
+    radius = request.args.get('radius')
 
-api.add_resource(restaurant, "/restaurants/<zip>")
+    specs = {
+      "zip": zip,
+      "radius": radius
+    }
+
+    return json.dumps(specs)
+
+api.add_resource(restaurant, "/restaurants")
 app.run(debug=True)
