92ce8642389ccb50d6157791ff881d67e7d8049d
dumbkid <dumbkid@localhost.localdomain>
2018-10-08 23:37:08 -0400

    sample to connect to yelp api with simple query of zip and radius

---
+++ sample-yelp-api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from yelpapi import YelpAPI
+
+import requests
+import myapi
+import json
+
+
+
+def main():
+
+  try:
+    yelp_api = YelpAPI(myapi.api_key(), timeout_s=3.0)
+    search_results = yelp_api.search_query(location='77042', radius='8000')
+
+  except:
+    print("error: ", sys.exc_info()[0])
+
+  blist = json.loads(json.dumps(search_results))
+
+  for business in blist["businesses"]:
+    print(business["name"].encode('ascii','ignore'))
+
+
+
+
+if __name__ == '__main__':
+  main()
+
